be9029a5bed270c38ba5b7ef37298a97f3d03b2c
bodhibroy <bodhi@RoyzzBox.(none)>
2012-05-14 22:20:37 +0800

    adding symlink-creator for creating vimrc and dotemacs

---
+++ symlink-creator.sh
@@ -0,0 +1,4 @@
+# creates symbolic links for my vimrc and .emacs
+#This file should exist in ~/sys/configs
+ln -s /home/bodhi/sys/configs/dotemacs /home/bodhi/.emacs
+ln -s /home/bodhi/sys/configs/vimrc /home/bodhi/.vimrc
