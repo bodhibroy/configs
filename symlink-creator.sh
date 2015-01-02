0464d87b6b7bdf9f77c76537633bfe49a3443e13
bodhi <bodhi@comp.nus.edu.sg>
2015-01-02 23:45:29 +0800

    properly set the symbolic link creator. Added the -F flag for force unlinking/removal and -v flag for verbosity.

--- symlink-creator.sh
+++ symlink-creator.sh
@@ -1,6 +1,4 @@
 # creates symbolic links for my vimrc and .emacs
-#This file should exist in ~/sys/configs
-var=$(pwd)
-echo $var
-ln -s $var/dotemacs.el $HOME/.emacs
-ln -s $var/vimrc $HOME/.vimrc
+# This file should exist in ~/sys/configs
+ln -sFv $(pwd)/dotemacs.el $HOME/.emacs
+ln -sFv $(pwd)/vimrc $HOME/.vimrc
