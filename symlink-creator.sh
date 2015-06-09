e15c8454b704ea1e1b468123531636f366eb10f0
Bodhisatta Barman Roy <bodhi@comp.nus.edu.sg>
2015-06-09 12:44:36 +0800

    modified i3 config to read i3status.conf from .i3/ directory, and changed the symlink creator accordingly

--- symlink-creator.sh
+++ symlink-creator.sh
@@ -3,5 +3,4 @@
 ln -sFv $(pwd)/dotemacs.el $HOME/.emacs
 ln -sFv $(pwd)/vimrc $HOME/.vimrc
 ln -sFv $(pwd)/i3 $HOME/.i3
-ln -sFv $(pwd)/i3status.conf $HOME/.i3status.conf
 ln -sFv $(pwd)/xmodmap $HOME/.xmodmap
